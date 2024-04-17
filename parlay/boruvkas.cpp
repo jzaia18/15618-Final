c5adb941e9c90b3b8f4f97d61a442c21aece32c3
Shubham Bhargava <shubhargava@gmail.com>
2024-04-17 01:50:12 -0400

    formatting and makefile fixes

--- parlay/boruvkas.cpp
+++ parlay/boruvkas.cpp
@@ -12,37 +12,10 @@
 // Driver
 // **************************************************************
 using vertex = int;
-using wtype = int;
-using weighted_graph =
-    parlay::sequence<parlay::sequence<std::pair<vertex, wtype>>>;
-
-// static weighted_graph read_graph_from_file_custom(const std::string& filename) {
-//     auto str = parlay::file_map(filename);
-//     auto tokens = parlay::tokens(str);
-//     long n = parlay::chars_to_long(tokens[0]);
-//     long m = parlay::chars_to_long(tokens[1]);
-//     if (tokens.size() != m * 3 + 2) {
-//         std::cout << "bad file format" << std::endl;
-//         exit(1);
-//     }
-//     weighted_graph G = parlay::tabulate(n, [&](long i) {
-//         return parlay::sequence<std::pair<vertex, wtype>>();
-//     });
-
-//     for (int i = 0; i < m; i++) {
-//         long u = parlay::chars_to_long(tokens[3 * i + 2]);
-//         long v = parlay::chars_to_long(tokens[3 * i + 3]);
-//         wtype w = parlay::chars_to_long(tokens[3 * i + 4]);
-//         G[u].push_back(std::make_pair(v, w));
-//         G[v].push_back(std::make_pair(u, w));
-//     }
-//     return G;
-// }
-
-
+using wtype = uint;
 
 int main(int argc, char* argv[]) {
-    auto usage = "Usage: boruvka <filename>";
+    auto usage = "Usage: boruvkas <filename>";
     if (argc != 2) {
         std::cout << usage << std::endl;
         exit(1);
@@ -50,18 +23,18 @@ int main(int argc, char* argv[]) {
 
     auto str = parlay::file_map(argv[1]);
     auto tokens = parlay::tokens(str);
-    long n = parlay::chars_to_long(tokens[0]);
-    long m = parlay::chars_to_long(tokens[1]);
+    uint n = parlay::chars_to_uint(tokens[0]);
+    uint m = parlay::chars_to_uint(tokens[1]);
     if (tokens.size() != m * 3 + 2) {
         std::cout << "bad file format" << std::endl;
         exit(1);
     }
-    edges<vertex, wtype> E;
+    parlay::sequence<edge<vertex, wtype>> E;
 
     for (int i = 0; i < m; i++) {
-        long u = parlay::chars_to_long(tokens[3 * i + 2]);
-        long v = parlay::chars_to_long(tokens[3 * i + 3]);
-        wtype w = parlay::chars_to_long(tokens[3 * i + 4]);
+        vertex u = parlay::chars_to_int(tokens[3 * i + 2]);
+        vertex v = parlay::chars_to_int(tokens[3 * i + 3]);
+        wtype w = parlay::chars_to_uint(tokens[3 * i + 4]);
         E.push_back(std::make_tuple(u, v, w));
     }
 
--- parlay/boruvkas.h
+++ parlay/boruvkas.h
@@ -4,28 +4,15 @@
 #include "parlaylib/include/parlay/primitives.h"
 #include "parlaylib/include/parlay/sequence.h"
 
-// struct Pair {
-//     std::uint32_t vert;
-//     std::uint32_t score;
-// };
+template <typename vertex, typename wtype>
+using edge = std::tuple<vertex, vertex, wtype>;
 
 template <typename vertex, typename wtype>
-using edges = parlay::sequence<std::tuple<vertex, vertex, wtype>>;
-
-template <typename vertex, typename wtype>
-auto boruvka(const edges<vertex, wtype>& E, std::uint32_t n) {
-    // auto verts =
-    //     parlay::tabulate<std::atomic<Pair>>(n, [&](long i) { return n; });
-
-    // auto verts = parlay::tabulate<std::atomic<Pair>>(n, [&](long i) {
-    //     Pair x = {n, UINT_MAX};
-    //     return x;
-    //     });
-
+auto boruvka(const parlay::sequence<edge<vertex, wtype>>& E, vertex n) {
     auto curr_edges = E;
     union_find<vertex> UF(n);
-    auto sol = parlay::tabulate<std::tuple<vertex, vertex, wtype>>(0, [](long i) { return std::make_tuple(-1,-1,-1); });
-
+    auto sol = parlay::tabulate<edge<vertex, wtype>>(
+        0, [](long i) { return std::make_tuple(-1, -1, -1); });
 
     while (curr_edges.size() > 0) {
         auto best_edge_index =
@@ -48,58 +35,45 @@ auto boruvka(const edges<vertex, wtype>& E, std::uint32_t n) {
             }
         });
 
-        // for (auto& x: best_edge_index) {
-        //     auto [u,v,w] = curr_edges[x];
-        //     std::cout << u << " " << v << " " << w << std::endl;
-        // }
+        sol = parlay::append(
+            sol, parlay::map_maybe(parlay::iota(n), [&](int i) {
+                int edge_index = best_edge_index[i];
+                if (edge_index == -1)
+                    return std::optional<edge<vertex, wtype>>{};
 
-        
+                auto [u, v, w] = curr_edges[edge_index];
+                // If this edge is covered twice, only union when i == u (u < v
+                // naturally)
+                if (i == v && edge_index == best_edge_index[u])
+                    return std::optional<edge<vertex, wtype>>{};
 
-        sol = parlay::append(sol, parlay::map_maybe(parlay::iota(n), [&](int i) {
-            int edge_index = best_edge_index[i];
-            if (edge_index == -1) return std::optional<std::tuple<vertex, vertex, wtype>>{};
+                // UF.link(UF.find(u), v);
+                bool done = false;
+                vertex root = -1;
+                auto un = u;
+                while (!done) {
+                    un = UF.find(un);
+                    done = UF.parents[un] == root &&
+                           UF.parents[un].compare_exchange_strong(root, v);
+                }
 
-            auto [u, v, w] = curr_edges[edge_index];
-            // If this edge is covered twice, only union when i == u (u < v
-            // naturally)
-            if (i == v && edge_index == best_edge_index[u]) return std::optional<std::tuple<vertex, vertex, wtype>>{};
+                // add edge
+                return std::optional{curr_edges[edge_index]};
+            }));
 
-            // UF.link(UF.find(u), v);
-            bool done = false;
-            int root = -1;
-            auto un = u;
-            while (!done) {
-                un = UF.find(un);
-                done = UF.parents[un] == root && UF.parents[un].compare_exchange_strong(root, v);
-            }
-            
-            
-
-            // add edge
-            return std::optional{curr_edges[edge_index]};
-            
-        }));
-
-        curr_edges = parlay::map_maybe(curr_edges, [&](const std::tuple<vertex, vertex, wtype>& x) {
-            auto [u, v, w] = x;
-            vertex un = UF.find(u);
-            vertex vn = UF.find(v);
-            return (un == vn) ? std::optional<std::tuple<vertex, vertex, wtype>>{}
-                                      : std::optional{std::make_tuple(un, vn, w)};
-        });
-
-        // for (auto& x: curr_edges) {
-        //     auto [u,v,w] = x;
-        //     std::cout << u << " " << v << " " << w << std::endl;
-        // }
-
-
-        //std::cout << curr_edges.size() << std::endl;
+        curr_edges =
+            parlay::map_maybe(curr_edges, [&](const edge<vertex, wtype>& x) {
+                auto [u, v, w] = x;
+                vertex un = UF.find(u);
+                vertex vn = UF.find(v);
+                return (un == vn) ? std::optional<edge<vertex, wtype>>{}
+                                  : std::optional{std::make_tuple(un, vn, w)};
+            });
     }
-    
+
     int t = 0;
-    for (auto [u,v, w]: sol) { 
-        t+= w;
+    for (auto [u, v, w] : sol) {
+        t += w;
     }
 
     std::cout << t << std::endl;
