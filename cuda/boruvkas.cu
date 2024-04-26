55a2ecd04a2cb9832228b125a584c251b03733e0
Jake Liam Zaia <jzaia@andrew.cmu.edu>
2024-04-25 20:36:31 -0400

    Make cuda version exec actual kernel (still no parallelism)

--- cuda/boruvkas.cu
+++ cuda/boruvkas.cu
@@ -37,7 +37,20 @@ __global__ void test(int *a) {
     a[threadID] = threadID;
 }
 
-inline int get_component(Vertex* componentlist, const int i) {
+// Define operators so that edges can be compared
+__device__ inline int edge_cmp(const Edge& lhs, const Edge& rhs)
+{
+    const int sub = lhs.weight - rhs.weight;
+    if (sub) {
+        return sub;
+    }
+    if (lhs.u == rhs.u) {
+        return (lhs.v - rhs.v);
+    }
+    return (lhs.u - rhs.u);
+}
+
+__device__ inline int get_component(Vertex* componentlist, const int i) {
     int curr = componentlist[i].component;
 
     while (componentlist[curr].component != curr) {
@@ -48,11 +61,11 @@ inline int get_component(Vertex* componentlist, const int i) {
     return curr;
 }
 
-inline void merge_components(Vertex* componentlist, const int i, const int j) {
+__device__ inline void merge_components(Vertex* componentlist, const int i, const int j) {
     componentlist[get_component(componentlist, i)].component = get_component(componentlist, j);
 }
 
-void assign_cheapest(Vertex* vertices, const Edge* edgelist, const int n_edges) {
+__device__ inline void assign_cheapest(Vertex* vertices, const Edge* edgelist, const int n_edges) {
     for (int i = 0; i < n_edges; i++) {
         const Edge& e = edgelist[i];
         int c1 = get_component(vertices, e.u);
@@ -64,16 +77,16 @@ void assign_cheapest(Vertex* vertices, const Edge* edgelist, const int n_edges) 
         }
 
         // Check if this edge is the cheapest (so far) for its connected components
-        if (vertices[c1].cheapest_edge == nullptr || e < *vertices[c1].cheapest_edge) {
+        if (vertices[c1].cheapest_edge == nullptr || edge_cmp(e, *vertices[c1].cheapest_edge) < 0) {
             vertices[c1].cheapest_edge = &e;
         }
-        if (vertices[c2].cheapest_edge == nullptr || e < *vertices[c2].cheapest_edge) {
+        if (vertices[c2].cheapest_edge == nullptr || edge_cmp(e, *vertices[c2].cheapest_edge) < 0) {
             vertices[c2].cheapest_edge = &e;
         }
     }
 }
 
-int update_mst(Vertex* vertices, const Edge* edgelist, MST& mst, const int n_vertices, const int n_edges) {
+__device__ inline int update_mst(Vertex* vertices, const Edge* edgelist, MST* mst, const int n_vertices, const int n_edges) {
     int n_unions = 0;
     // Connect newest edges to MST
     for (int i = 0; i < n_vertices; i++) {
@@ -86,7 +99,7 @@ int update_mst(Vertex* vertices, const Edge* edgelist, MST& mst, const int n_ver
         //     continue;
         // }
 
-        mst.mst[mst.size++] = *edge_ptr;
+        mst->mst[mst->size++] = *edge_ptr;
         vertices[get_component(vertices, edge_ptr->u)].cheapest_edge = nullptr;
         vertices[get_component(vertices, edge_ptr->v)].cheapest_edge = nullptr;
         merge_components(vertices, edge_ptr->u, edge_ptr->v);
@@ -95,29 +108,30 @@ int update_mst(Vertex* vertices, const Edge* edgelist, MST& mst, const int n_ver
     return n_unions;
 }
 
-MST boruvka_mst_helper(const int n_vertices, const int n_edges, const Edge* edgelist) {
-    MST mst;
-    mst.size = 0;
-    mst.capacity = n_vertices-1;
-    mst.mst = (Edge*) malloc(sizeof(Edge) * (n_vertices-1));
-    Vertex* vertices = (Vertex*) malloc(sizeof(Vertex) * n_vertices);
+__global__ void boruvka_mst_helper(const int n_vertices,
+                                   const int n_edges,
+                                   const Edge* edgelist,
+                                   Vertex* vertices,
+                                   MST* mst) {
+    int threadID = threadIdx.y * blockDim.x + threadIdx.x;
 
-    // initialize components
-    for (int i = 0; i < n_vertices; i++) {
-        vertices[i] = Vertex{i, nullptr};
+    if (threadID == 0) {
+
+        // initialize components
+        for (int i = 0; i < n_vertices; i++) {
+            vertices[i] = Vertex{i, nullptr};
+        }
+
+        int n_components = n_vertices;
+        int diff;
+
+        do {
+            assign_cheapest(vertices, edgelist, n_edges);
+
+            diff = update_mst(vertices, edgelist, mst, n_vertices, n_edges);
+            n_components -= diff;
+        } while (diff != 0 && n_components > 1);
     }
-
-    int n_components = n_vertices;
-    int diff;
-
-    do {
-        assign_cheapest(vertices, edgelist, n_edges);
-
-        diff = update_mst(vertices, edgelist, mst, n_vertices, n_edges);
-        n_components -= diff;
-    } while (diff != 0 && n_components > 1);
-
-    return mst;
 }
 
 MST boruvka_mst(const int n_vertices, const int n_edges, const Edge* edgelist) {
@@ -188,14 +202,42 @@ MST boruvka_mst(const int n_vertices, const int n_edges, const Edge* edgelist) {
         cudaFree(d_a);
     }
 
-    MST result = boruvka_mst_helper(n_vertices, n_edges, edgelist);
+    MST mst;
+    mst.size = 0;
+    mst.capacity = n_vertices-1;
+    mst.weight = 0;
+    Edge* mst_tree = (Edge*) malloc(sizeof(Edge) * (n_vertices-1));
+
+    MST* device_mst;
+    Edge* device_mst_tree;
+    Vertex* device_vertices;
+    Edge* device_edgelist;
+
+    cudaMalloc(&device_mst, sizeof(MST));
+    cudaMalloc(&device_mst_tree, sizeof(Edge) * (n_vertices-1));
+    cudaMalloc(&device_vertices, sizeof(Vertex) * n_vertices);
+    cudaMalloc(&device_edgelist, sizeof(Edge) * n_edges);
+
+    cudaMemcpy(device_edgelist, edgelist, sizeof(Edge) * n_edges, cudaMemcpyHostToDevice);
+    mst.mst = device_mst_tree;
+    cudaMemcpy(device_mst, &mst, sizeof(MST), cudaMemcpyHostToDevice);
+
+    boruvka_mst_helper<<<1, BLOCKSIZE>>>(n_vertices, n_edges, device_edgelist, device_vertices, device_mst);
+
+    cudaMemcpy(&mst, device_mst, sizeof(MST), cudaMemcpyDeviceToHost);
+    cudaMemcpy(mst_tree, device_mst_tree, sizeof(Edge) * mst.size, cudaMemcpyDeviceToHost);
+    mst.mst = mst_tree;
+
+    cudaFree(device_mst);
+    cudaFree(device_mst_tree);
+    cudaFree(device_vertices);
+    cudaFree(device_edgelist);
 
     // TODO: Move this into the kernel
-    result.weight = 0;
-    for (int i = 0; i < result.size; i++) {
-        const Edge& e = result.mst[i];
-        result.weight += e.weight;
+    for (int i = 0; i < mst.size; i++) {
+        const Edge& e = mst.mst[i];
+        mst.weight += e.weight;
     }
 
-    return result;
+    return mst;
 }
--- cuda/boruvkas.h
+++ cuda/boruvkas.h
@@ -4,38 +4,6 @@ struct Edge {
     int u;
     int v;
     int weight;
-
-    // Define operators so that edges can be compared
-    bool operator <(const Edge& rhs) const
-    {
-        if (this->weight < rhs.weight) {
-            return true;
-        }
-        if (this->weight == rhs.weight) {
-            if (this->u == rhs.u) {
-                return (this->v < rhs.v);
-            }
-            return (this->u < rhs.u);
-        }
-        return false;
-    }
-    bool operator >(const Edge& rhs) const
-    {
-        if (this->weight > rhs.weight) {
-            return true;
-        }
-        if (this->weight == rhs.weight) {
-            if (this->u == rhs.u) {
-                return (this->v > rhs.v);
-            }
-            return (this->u > rhs.u);
-        }
-        return false;
-    }
-    // bool operator ==(const Edge& rhs) const
-    // {
-    //     return this->u == rhs.u && this->v == rhs.v && this->weight == rhs.weight;
-    // }
 };
 
 struct Vertex {
