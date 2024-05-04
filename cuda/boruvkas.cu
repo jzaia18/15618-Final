5b1da2237cfbbb814b0b5cdd62a00b881bd539c5
Shubham Bhargava <shubhamb@ghc63.ghc.andrew.cmu.edu>
2024-05-03 22:17:17 -0400

    refactor

--- cuda/boruvkas.cu
+++ cuda/boruvkas.cu
@@ -12,9 +12,9 @@
 
 // TODO: Is it even helpful to think of this as a block for this problem?
 // Define constants for CUDA threadblocks
-#define THREADBLOCK_WIDTH (8)
-#define THREADBLOCK_HEIGHT (8)
-#define BLOCKSIZE (THREADBLOCK_WIDTH * THREADBLOCK_HEIGHT)
+#define BLOCKSIZE 64
+#define BLOCKCOUNT 64
+#define THREADCOUNT (BLOCKSIZE * BLOCKCOUNT)
 
 #define NO_EDGE (INT_MAX)
 
@@ -24,7 +24,11 @@
 
 // This stores the global constants
 struct GlobalConstants {
-    // TODO vertices and edges should be here
+    Vertex* vertices;
+    Edge* edges;
+    int* mst_tree;
+    int n_vertices;
+    int n_edges;
 };
 
 // Another global value
@@ -35,7 +39,7 @@ __device__ int n_components;
 // be stored in special "constant" memory on the GPU. (we didn't talk
 // about this type of memory in class, but constant memory is a fast
 // place to put read-only variables).
-__constant__ GlobalConstants cuConstRendererParams;
+__constant__ GlobalConstants cuConstGraphParams;
 
 __device__ inline int get_component(Vertex* componentlist, const int i) {
     int curr = componentlist[i].component;
@@ -58,39 +62,42 @@ __device__ inline void merge_components(Vertex* componentlist, const int i,
     } while (old != u);
 }
 
-__global__ void init_arrs(const int n_vertices, Vertex* vertices) {
-    const int threadID = threadIdx.y * blockDim.x + threadIdx.x;
+__global__ void init_arrs() {
+    const int threadID = threadIdx.x;
 
-    const int start = (threadID * n_vertices / BLOCKSIZE);
-    const int end = ((threadID + 1) * n_vertices / BLOCKSIZE);
-
-    // initialize components
-    for (int i = start; i < end; i++) {
-        vertices[i] = Vertex{i, NO_EDGE};
-    }
-}
-
-__global__ void reset_arrs(const int n_vertices, Vertex* vertices) {
-    const int threadID = threadIdx.y * blockDim.x + threadIdx.x;
-
-    const int start = (threadID * n_vertices / BLOCKSIZE);
-    const int end = ((threadID + 1) * n_vertices / BLOCKSIZE);
+    const int start = (threadID * cuConstGraphParams.n_vertices) / BLOCKSIZE;
+    const int end =
+        ((threadID + 1) * cuConstGraphParams.n_vertices) / BLOCKSIZE;
 
     // initialize components
     for (int i = start; i < end; i++) {
-        vertices[i].cheapest_edge = NO_EDGE;
+        cuConstGraphParams.vertices[i] = Vertex{i, NO_EDGE};
     }
 }
 
-__global__ void assign_cheapest(Vertex* vertices, Edge* edgelist,
-                                const int n_edges) {
-    const int threadID = threadIdx.y * blockDim.x + threadIdx.x;
+__global__ void reset_arrs() {
+    const int threadID = threadIdx.x;
 
-    const int start = (threadID * n_edges) / BLOCKSIZE;
-    const int end = ((threadID + 1) * n_edges) / BLOCKSIZE;
+    const int start = (threadID * cuConstGraphParams.n_vertices) / BLOCKSIZE;
+    const int end =
+        ((threadID + 1) * cuConstGraphParams.n_vertices) / BLOCKSIZE;
+
+    // initialize components
+    for (int i = start; i < end; i++) {
+        cuConstGraphParams.vertices[i].cheapest_edge = NO_EDGE;
+    }
+}
+
+__global__ void assign_cheapest() {
+    const int threadID = threadIdx.x + blockIdx.x * blockDim.x;
+
+    const int start = (threadID * cuConstGraphParams.n_edges) / THREADCOUNT;
+    const int end = ((threadID + 1) * cuConstGraphParams.n_edges) / THREADCOUNT;
+
+    Vertex* vertices = cuConstGraphParams.vertices;
 
     for (int i = start; i < end; i++) {
-        Edge& e = edgelist[i];
+        Edge& e = cuConstGraphParams.edges[i];
         e.u = get_component(vertices, e.u);
         e.v = get_component(vertices, e.v);
 
@@ -101,44 +108,47 @@ __global__ void assign_cheapest(Vertex* vertices, Edge* edgelist,
 
         // Atomic update cheapest_edge[u]
         int old = vertices[e.u].cheapest_edge;
-        while (old == NO_EDGE || e.weight < edgelist[old].weight) {
+        while (old == NO_EDGE ||
+               e.weight < cuConstGraphParams.edges[old].weight) {
             old = atomicCAS(&vertices[e.u].cheapest_edge, old, i);
         }
 
         // Atomic update cheapest_edge[v]
         old = vertices[e.v].cheapest_edge;
-        while (old == NO_EDGE || e.weight < edgelist[old].weight) {
+        while (old == NO_EDGE ||
+               e.weight < cuConstGraphParams.edges[old].weight) {
             old = atomicCAS(&vertices[e.v].cheapest_edge, old, i);
         }
     }
 }
 
-__global__ void update_mst(Vertex* vertices, const Edge* edgelist, MST* mst,
-                           const int n_vertices, const int n_edges) {
-    const int threadID = threadIdx.y * blockDim.x + threadIdx.x;
+__global__ void update_mst() {
+    const int threadID = threadIdx.x;
 
-    const int start = (threadID * n_vertices) / BLOCKSIZE;
-    const int end = ((threadID + 1) * n_vertices) / BLOCKSIZE;
+    const int start = (threadID * cuConstGraphParams.n_vertices) / BLOCKSIZE;
+    const int end =
+        ((threadID + 1) * cuConstGraphParams.n_vertices) / BLOCKSIZE;
 
     int n_unions = 0;
     // Connect newest edges to MST
     for (int i = start; i < end; i++) {
-        const int edge_ind = vertices[i].cheapest_edge;
+        const int edge_ind = cuConstGraphParams.vertices[i].cheapest_edge;
 
         if (edge_ind == NO_EDGE) {
             continue;
         }
 
-        const Edge& edge_ptr = edgelist[edge_ind];
+        const Edge& edge_ptr = cuConstGraphParams.edges[edge_ind];
 
         // If this edge is covered twice, only union when i == u (u < v is
         // assumed)
-        if (edge_ptr.v == i && edge_ind == vertices[edge_ptr.u].cheapest_edge) {
+        if (edge_ptr.v == i &&
+            edge_ind == cuConstGraphParams.vertices[edge_ptr.u].cheapest_edge) {
             continue;
         }
 
-        mst->mst[edge_ind] = 1;
-        merge_components(vertices, edge_ptr.u, edge_ptr.v);
+        cuConstGraphParams.mst_tree[edge_ind] = 1;
+        merge_components(cuConstGraphParams.vertices, edge_ptr.u, edge_ptr.v);
         n_unions++;
     }
 
@@ -146,7 +156,7 @@ __global__ void update_mst(Vertex* vertices, const Edge* edgelist, MST* mst,
     atomicSub(&n_components, n_unions);
 }
 
-void initGPUs() {
+MST boruvka_mst(const int n_vertices, const int n_edges, const Edge* edgelist) {
     int deviceCount = 0;
     bool isFastGPU = false;
     std::string name;
@@ -179,77 +189,76 @@ void initGPUs() {
             "NVIDIA RTX 2080.\n");
         printf("---------------------------------------------------------\n");
     }
-}
 
-MST boruvka_mst(const int n_vertices, const int n_edges, const Edge* edgelist) {
-    MST mst;
-    mst.weight = 0;
-    int* mst_tree = (int*)malloc(sizeof(int) * n_edges);
-
-    MST* device_mst;
     int* device_mst_tree;
     Vertex* device_vertices;
     Edge* device_edgelist;
 
-    cudaMalloc(&device_mst, sizeof(MST));
     cudaMalloc(&device_mst_tree, sizeof(int) * n_edges);
-    cudaMalloc(&device_vertices, sizeof(Vertex) * n_vertices);
-    cudaMalloc(&device_edgelist, sizeof(Edge) * n_edges);
-
-    cudaMemcpy(device_edgelist, edgelist, sizeof(Edge) * n_edges,
-               cudaMemcpyHostToDevice);
-    mst.mst = device_mst_tree;
-    cudaMemcpy(device_mst, &mst, sizeof(MST), cudaMemcpyHostToDevice);
     cudaMemset(device_mst_tree, 0, sizeof(int) * n_edges);
 
-    // Run Boruvka's in parallel
+    cudaMalloc(&device_vertices, sizeof(Vertex) * n_vertices);
 
+    cudaMalloc(&device_edgelist, sizeof(Edge) * n_edges);
+    cudaMemcpy(device_edgelist, edgelist, sizeof(Edge) * n_edges,
+               cudaMemcpyHostToDevice);
+
+    GlobalConstants params;
+    params.vertices = device_vertices;
+    params.edges = device_edgelist;
+    params.mst_tree = device_mst_tree;
+    params.n_edges = n_edges;
+    params.n_vertices = n_vertices;
+
+    cudaMemcpyToSymbol(cuConstGraphParams, &params, sizeof(GlobalConstants));
+
+    // Run Boruvka's in parallel
     int n_comp = n_vertices;
     int n_comp_old;
 
     // Initialise global
     cudaMemcpyToSymbol(n_components, &n_comp, sizeof(int));
 
-    init_arrs<<<1, BLOCKSIZE>>>(n_vertices, device_vertices);
+    init_arrs<<<1, BLOCKSIZE>>>();
 
     do {
         n_comp_old = n_comp;
 
-        reset_arrs<<<1, BLOCKSIZE>>>(n_vertices, device_vertices);
-        assign_cheapest<<<1, BLOCKSIZE>>>(device_vertices, device_edgelist,
-                                          n_edges);
-        update_mst<<<1, BLOCKSIZE>>>(device_vertices, device_edgelist,
-                                     device_mst, n_vertices, n_edges);
+        reset_arrs<<<1, BLOCKSIZE>>>();
+        assign_cheapest<<<BLOCKCOUNT, BLOCKSIZE>>>();
+        update_mst<<<1, BLOCKSIZE>>>();
         cudaMemcpyFromSymbol(&n_comp, n_components, sizeof(int));
 
         // debug
         // Vertex * ts = (Vertex *) malloc(sizeof(Vertex) * n_vertices);
-        // cudaMemcpy(ts, device_vertices, sizeof(Vertex) * n_vertices, cudaMemcpyDeviceToHost);
-        // for (int i = 0; i < n_vertices; i++) {
+        // cudaMemcpy(ts, device_vertices, sizeof(Vertex) * n_vertices,
+        // cudaMemcpyDeviceToHost); for (int i = 0; i < n_vertices; i++) {
         //     printf("%d ", ts[i].cheapest_edge);
         // }
         // printf("\n");
         // Edge * ed = (Edge *) malloc(sizeof(Edge) * n_vertices);
-        // cudaMemcpy(ed, device_edgelist, sizeof(Edge) * n_edges, cudaMemcpyDeviceToHost);
-        // for (int i = 0; i < n_edges; i++) {
+        // cudaMemcpy(ed, device_edgelist, sizeof(Edge) * n_edges,
+        // cudaMemcpyDeviceToHost); for (int i = 0; i < n_edges; i++) {
         //     printf("%d-%d-%d ", ed[i].u, ed[i].v, ed[i].weight);
         // }
         // printf("nc %d\n", n_comp);
     } while (n_comp != n_comp_old && n_comp > 1);
 
+    MST mst;
+    mst.weight = 0;
+    int* mst_tree = (int*)malloc(sizeof(int) * n_edges);
+
     // Copy run results off of device
-    cudaMemcpy(&mst, device_mst, sizeof(MST), cudaMemcpyDeviceToHost);
     cudaMemcpy(mst_tree, device_mst_tree, sizeof(int) * n_edges,
                cudaMemcpyDeviceToHost);
     mst.mst = mst_tree;
 
     // Clean up device memory
-    cudaFree(device_mst);
     cudaFree(device_mst_tree);
     cudaFree(device_vertices);
     cudaFree(device_edgelist);
 
-    // TODO: Move this into the kernel that performs a scan
+    // TODO: Move this into the kernel (filtering vertices to get a short list)
     // Compute final weight
     for (int i = 0; i < n_edges; i++) {
         if (mst.mst[i] == 1) {
